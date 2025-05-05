910edd27314808f0d383fc51cbe2c761032746dd
Elena7939 <dolgikh.helen@bk.ru>
2025-05-05 16:00:19 +0400

    дз 3 готова

--- Homework1/index.js
+++ Homework1/index.js
@@ -63,3 +63,120 @@
 
 // let name = prompt('Как Вас зовут?');
 // alert(`Привет,  ${name}`);
+
+
+
+// // Домашнее задание 3
+
+
+// //Задание 1//
+
+// let password = 'дуб';
+// let version = prompt('Введите пароль');
+// (password === version) ? alert('Пароль введен верно') : alert('Пароль введен неправильно')
+
+// //Задание 2//
+
+// let c = Number(prompt('Напишите любое число'));
+// (c > 0 && c < 10) ? alert('Верно') : alert('Неверно');
+
+
+// //Задание 3//
+
+
+// let d = Number(prompt("Введите первое число"));
+// let e = Number(prompt("Введите второе число"));
+// ((d > 100)||(e > 100)) ? alert('Верно') : alert('Неверно');
+
+
+
+// // //Задание 4//
+
+
+// let a = '2';
+// let b = '3';
+// let sum = Number(a) + Number(b);
+// alert(sum);
+
+
+// // //Задание 5//
+
+
+// let = monthNumber = prompt("Введите номер месяца");
+// switch (monthNumber) {
+//     case ('1'):
+//     alert('Зима');
+//         break;
+
+//     case ('2'):
+//     alert('Зима');
+//         break;
+
+//     case ('12'):
+//     alert('Зима');
+//         break;    
+    
+//      case ('3'):
+//     alert('Весна');
+//         break;
+
+//     case ('4'):
+//     alert('Весна');
+//         break;
+
+//     case ('5'):
+//     alert('Весна');
+//         break;
+    
+//     case ('6'):
+//     alert('Лето');
+//         break;
+
+//     case ('7'):
+//     alert('Лето');
+//         break;
+        
+//     case ('8'):
+//     alert('Лето');
+//         break;
+    
+//     case ('9'):
+//     alert('Осень');
+//         break;
+
+//     case ('10'):
+//     alert('Осень');
+//         break;
+        
+//     case ('11'):
+//     alert('Осень');
+//         break;
+
+//     default:
+//     alert('С КАКОЙ СТАТИ ВЫ МЕНЯ ИЗВИНИТЕ Я СКАНДАЛ ТАКОЙ УЧИНЮ')
+//         break;
+// }
+// ///////////////https://vk.com/video384860982_456241210
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Задание 10//
+
+
+// //Задание 10//
